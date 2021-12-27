c1e019955445d5ef27656a9c08d2551d17d0b2b4
tangivan <38152920+tangivan@users.noreply.github.com>
2021-12-26 17:44:27 -0800

    Add SnippetList starter component

---
+++ src/components/SnippetList.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const SnippetList = () => {
+  return (
+    <div className="container col-span-2">
+      <p>I am the snippet list</p>
+    </div>
+  );
+};
+
+export default SnippetList;
