9593d5831b329042056bd096a1754baacd42d652
tangivan <38152920+tangivan@users.noreply.github.com>
2021-12-26 17:45:24 -0800

    Setup routes and lazy loading

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
-import Signup from "./pages/Signup";
+import UserContext from "./context/user";
+import useAuthListener from "./hooks/useAuthListener";
 import "./styles/tailwind.css";
 
 const Login = lazy(() => import("./pages/Login"));
 const SignUp = lazy(() => import("./pages/Signup"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const NotFound = lazy(() => import("./pages/Notfound"));
 
 function App() {
+  const { user } = useAuthListener();
+
   return (
-    <Router>
-      <Suspense fallback={<p>Loading ...</p>}>
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} />
-          <Route path={ROUTES.SIGN_UP} component={SignUp} />
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading ...</p>}>
+          <Switch>
+            <Route path={ROUTES.LOGIN} component={Login} exact />
+            <Route path={ROUTES.SIGN_UP} component={SignUp} exact />
+            <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
