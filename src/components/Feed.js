a3a4620541c8707265551a674e944c7538d5326f
tangivan <38152920+tangivan@users.noreply.github.com>
2022-01-07 00:57:06 -0800

    Refactor Feed.js to use LoggedInUserContext

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,24 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import LoggedInUserContext from "../context/logged-in-user";
 import useSnippets from "../hooks/useSnippets";
 import Post from "./Post";
 
 const Feed = () => {
-  const { snippets } = useSnippets();
-  console.log("snippets", snippets);
+  const { user } = useContext(LoggedInUserContext);
+  const { snippets } = useSnippets(user);
   return (
     <div className="container col-span-2">
       {!snippets ? (
-        <>
-          <Skeleton count={4} width={640} height={500} className="mb-5" />
-        </>
-      ) : snippets?.length > 0 ? (
+        <Skeleton count={4} width={640} height={500} className="mb-5" />
+      ) : (
         snippets.map((content) => (
           <Post key={content.docId} content={content} />
         ))
-      ) : (
-        <p className="text-center text-2xl">Follow people to see snippets</p>
       )}
     </div>
   );
