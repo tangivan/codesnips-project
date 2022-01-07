9e6b8ffc82c7ac461708f93d60e46edcf76a3f8b
tangivan <38152920+tangivan@users.noreply.github.com>
2022-01-07 01:05:04 -0800

    Implement the Profile page

---
+++ src/pages/Profile.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { getUserByUsername } from "../services/firebase";
+import * as ROUTES from "../constants/routes";
+import Header from "../components/Header";
+import UserProfile from "../components/Profile/";
+
+const Profile = () => {
+  const { username } = useParams();
+  const [user, setUser] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    async function checkUserExists() {
+      const [user] = await getUserByUsername(username);
+      if (user.userId) {
+        setUser(user);
+      } else {
+        history.push(ROUTES.NOT_FOUND);
+      }
+    }
+
+    checkUserExists();
+  }, [username, history]);
+
+  return user?.username ? (
+    <div className="bg-gray-background">
+      <Header />
+      <div className="mx-auto max-w-screen-leg">
+        <UserProfile user={user} />
+      </div>
+    </div>
+  ) : null;
+};
+
+export default Profile;
