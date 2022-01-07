b613b10ddbb8304eebb6bfd1aef89108a5c9f7b0
tangivan <38152920+tangivan@users.noreply.github.com>
2022-01-07 01:05:42 -0800

    Refactor useSnippets to take in a user instead of using useContext

--- src/hooks/useSnippets.js
+++ src/hooks/useSnippets.js
@@ -1,28 +1,24 @@
-import React, { useState, useEffect, useContext } from "react";
-import UserContext from "../context/user";
-import { getSnippets, getUserByUserId } from "../services/firebase";
+import { useState, useEffect } from "react";
+import { getSnippets } from "../services/firebase";
 
-const useSnippets = () => {
+const useSnippets = (user) => {
   const [snippets, setSnippets] = useState(null);
-  const {
-    user: { uid: userId = "" },
-  } = useContext(UserContext);
 
   useEffect(() => {
     async function getFeedSnippets() {
-      const [{ following }] = await getUserByUserId(userId);
-      let followedUserSnippets = [];
-
       //does this uers follow ppl?
-      if (following.length > 0) {
-        followedUserSnippets = await getSnippets(userId, following);
+      if (user?.following?.length > 0) {
+        const followedUserSnippets = await getSnippets(
+          user.userId,
+          user.following
+        );
+        // re-arrange snippets
+        followedUserSnippets.sort((a, b) => b.dateCreated - a.dateCreated);
+        setSnippets(followedUserSnippets);
       }
-
-      followedUserSnippets.sort((a, b) => b.dateCreated - a.dateCreated);
-      setSnippets(followedUserSnippets);
     }
     getFeedSnippets();
-  }, [userId]);
+  }, [user.userId]);
   return { snippets };
 };
 
