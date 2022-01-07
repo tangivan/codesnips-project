344760b35971f57e279fb23ca2507ee7ea776545
tangivan <38152920+tangivan@users.noreply.github.com>
2022-01-07 01:03:33 -0800

    Refactor Dashboard to use LoggedInUserContext with useUser

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,26 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
 import Feed from "../components/Feed";
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
 import SideBar from "../components/Sidebar";
 import SnippetList from "../components/SnippetList";
+import useUser from "../hooks/useUser";
+import LoggedInUserContext from "../context/logged-in-user";
 
-const Dashboard = () => {
+const Dashboard = ({ user: loggedInUser }) => {
+  const { user } = useUser(loggedInUser.uid);
   useEffect(() => {
     document.title = "Codesnips";
   }, []);
 
   return (
-    <div className="bg-gray-background">
-      <Header />
-      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
-        {/* <SearchBar /> */}
-        <Feed />
-        <SideBar />
-        {/* <SnippetList /> */}
+    <LoggedInUserContext.Provider value={{ user }}>
+      <div className="bg-gray-background">
+        <Header />
+        <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+          {/* <SearchBar /> */}
+          <Feed />
+          <SideBar />
+          {/* <SnippetList /> */}
+        </div>
       </div>
-    </div>
+    </LoggedInUserContext.Provider>
   );
+};
+
+Dashboard.propTypes = {
+  user: PropTypes.object.isRequired,
 };
 
 export default Dashboard;
