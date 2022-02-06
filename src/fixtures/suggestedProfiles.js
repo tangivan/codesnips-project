06be4792b80c163779e5857d503802cf3364fefa
tangivan <38152920+tangivan@users.noreply.github.com>
2022-02-05 21:46:14 -0800

    Add suggestedProfiles fixtures for test mocking

---
+++ src/fixtures/suggestedProfiles.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    followers: [],
+    fullName: "Adam",
+    dateCreated: 1639731618139,
+    following: [],
+    userId: "Rek5saJNA5Rt2bxGhVXrw6Cluhm1",
+    emailAddress: "adam@adam.com",
+    username: "Learth",
+    docId: "utH4EadD3gBUbQkdG6Da",
+  },
+];
