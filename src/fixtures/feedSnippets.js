e490f80b81d988586c33a0342df4889717ac8083
tangivan <38152920+tangivan@users.noreply.github.com>
2022-02-05 21:46:31 -0800

    Add feedSnippets fixture for test mocking

---
+++ src/fixtures/feedSnippets.js
@@ -0,0 +1,53 @@
+export default [
+  {
+    username: "Magikarpet",
+    snippetId: 11,
+    comments: [
+      {
+        displayName: "ivannn",
+        comment: "hadfsfdsd",
+      },
+    ],
+    likes: ["aVbFRSQ6axXBBTXH6dMPmnSPERl1"],
+    language: "html",
+    title: "tailwind cdn",
+    snippet: '<script src="https://cdn.tailwindcss.com"></script>',
+    userId: "4",
+    dateCreated: 1640732380761,
+    docId: "KfigfFk9ZJfBty6yMm1U",
+  },
+  {
+    username: "SleepyTeas",
+    snippetId: 11,
+    comments: [
+      {
+        displayName: "Magikarpet",
+        comment: "Whoa, how do you use it?",
+      },
+      {
+        displayName: "Learth",
+        comment: "Nice.",
+      },
+    ],
+    likes: [],
+    language: "javascript",
+    title: "hello world",
+    snippet: 'console.log("hello world");',
+    userId: "2",
+    dateCreated: 1639731618154,
+    docId: "jX6LjukP6Gn3hHkZflzn",
+  },
+  {
+    username: "ivannn",
+    likes: [],
+    language: "css",
+    title: "status loading animation",
+    snippet:
+      "&__status-image {    margin-right: 2rem;    animation: rotate 1s linear infinite;  }",
+    snippetId: 3,
+    comments: [],
+    userId: "aVbFRSQ6axXBBTXH6dMPmnSPERl1",
+    dateCreated: 1639731618154,
+    docId: "nZUDNXCnheL6AKJYLieo",
+  },
+];
