bc23198b12e205d59bb4493e7a097c84fa3b1d95
tangivan <38152920+tangivan@users.noreply.github.com>
2022-02-05 21:46:57 -0800

    Add loggedInUser fixture for test mocking

---
+++ src/fixtures/loggedInUser.js
@@ -0,0 +1,9 @@
+export default {
+  userId: "aVbFRSQ6axXBBTXH6dMPmnSPERl1",
+  username: "ivannn",
+  fullName: "ivan t",
+  emailAddress: "ivan3@ivan.com",
+  following: ["2", "4"],
+  followers: [],
+  dateCreated: 1613097964107,
+};
